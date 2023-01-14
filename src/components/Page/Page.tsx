290f4595cf87abb340086eb11282de3c66fd69ba
Fatih Toker <fatihhtoker@gmail.com>
2023-01-14 14:02:44 +0100

    change data

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen container mx-auto px-16 py-4">{children}</div>
+    <div className="container mx-auto px-16 py-4">{children}</div>
   );
 }
